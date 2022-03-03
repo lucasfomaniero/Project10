304ba70116b36c9deea62f0affeaea0b996cf0b2
Lucas Maniero <lucasfomaniero@gmail.com>
2022-03-02 21:37:25 -0300

    Created a detail view controller to change the photo and name instead of an alert

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -7,46 +7,29 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     let cellID = "personCell"
     var people = [Person]()
     var selectedItem: IndexPath?
-    
-    lazy var renameAlert: UIAlertController = {
-        let alert = UIAlertController(title: "Rename person", message: "Type the new name:", preferredStyle: .alert)
-        alert.addTextField()
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: renamePerson)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alert.addAction(okAction)
-        alert.addAction(cancelAction)
-        print("✅ Created the renameAlert")
-        return alert
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .systemBackground
         title = "Contacts"
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAddPersonController))
         navigationItem.rightBarButtonItem = addButton
         collectionView.register(PersonCollectionViewCell.self, forCellWithReuseIdentifier: cellID)
         collectionView.contentInset = UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10)
 
     }
     
-    func renamePerson(_ action: UIAlertAction?) {
-        guard let name = renameAlert.textFields?[0].text, let index = selectedItem else {return}
-        people[index.item].name = name
-        renameAlert.textFields?[0].text = ""
-        collectionView.reloadData()
+    @objc func showAddPersonController(){
+        let addPersonVC = AddPersonViewController()
+        addPersonVC.delegate = self
+        navigationController?.pushViewController(addPersonVC, animated: true)
     }
-    
-    @objc func addNewPerson() {
-        let pickerController = UIImagePickerController()
-        pickerController.allowsEditing = true
-        pickerController.delegate = self
-        present(pickerController, animated: true, completion: nil)
-    }
+
+  
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? PersonCollectionViewCell else {
             fatalError("Unable to dequeue PersonCell.")
@@ -69,45 +52,46 @@ class ViewController: UICollectionViewController {
         collectionView.reloadData()
     }
     
-
-}
-
-extension ViewController: UICollectionViewDelegateFlowLayout {
     
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        selectedItem = indexPath
-        present(renameAlert, animated: true, completion: nil)
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: 120, height: 140 )
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 8
-    }
-
-    
-}
-
-extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard let image = info[.editedImage] as? UIImage else { return }
-
-        let imageName = UUID().uuidString
-        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
-
-        if let jpegData = image.jpegData(compressionQuality: 0.8) {
-            try? jpegData.write(to: imagePath)
-        }
-        
-        createNewPerson(imageName)
-
-        dismiss(animated: true)
-    }
-
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
     }
+    
+
 }
+
+extension ViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let personVC = AddPersonViewController()
+        personVC.person = people[indexPath.item]
+        present(personVC, animated: true, completion: nil)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = view.frame
+        return .init(width: size.width - 32.0, height: size.height / 4)
+//        return .init(width: 120, height: 140 )
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    
+}
+
+protocol AddPersonDelegate: AnyObject {
+    func shouldSaveANewPerson(_ person: Person)
+}
+
+extension ViewController: AddPersonDelegate {
+    func shouldSaveANewPerson(_ person: Person) {
+        people.append(person)
+        collectionView.reloadData()
+    }
+}
+
+
 
